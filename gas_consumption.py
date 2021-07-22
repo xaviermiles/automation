0b18b28f634dad8e578ed1b635287c2b16fcd35b
Xavier Miles <xavier.miles@stats.govt.nz>
2021-07-22 04:50:41 +0000

    Add scripts

---
+++ gas_consumption.py
@@ -0,0 +1,144 @@
+import os
+import re
+from datetime import datetime
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait, Select
+
+
+def get_driver():
+    opts = webdriver.FirefoxOptions()
+    opts.add_argument("--headless")
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.manager.showWhenStarting", False)
+    profile.set_preference("browser.download.dir", OUTPUT_FOLDER)
+    download_filetypes = ("application/octet-stream"
+                          ",application/download; charset=utf-8")
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", 
+                           download_filetypes)
+    driver = webdriver.Firefox(options=opts, firefox_profile=profile)
+    return driver
+
+
+def by_largest_users():
+    print("Starting: by_largest_users")
+    driver = get_driver()
+    driver.get(
+        "https://www.oatis.co.nz/Ngc.Oatis.UI.Web.Internet/Common/"
+        "OatisLogin.aspx"
+    )
+    # 1. Click 'Maui'
+    driver.find_element_by_id('MauiIX').click()
+    # 2. Go to 'Publications'
+    driver.find_element_by_id('HeaderMenuControl1_LinkPublications').click()
+    # 3. Download latest report in 'Daily Metered and Scheduled Quantity Report',
+    #    replacing the previous version
+    report_sections = driver.find_elements_by_xpath(
+        "//table[@id = 'DocumentCategoryDataList']"
+        "//span[contains(@id, 'DocumentCategoryLabel')]"
+    )
+    section_num = None
+    for section in report_sections:
+        if section.text == 'Daily Metered and Scheduled Quantity Report':
+            section_id = section.get_attribute('id')
+            section_num = re.match(
+                r'DocumentCategoryDataList__ctl(\d+)_DocumentCategoryLabel',
+                section_id
+            ).group(1)
+            break
+    if section_num is None:
+        raise NotImplementedError("Section with Maui SQMQ reports not found.")
+    
+    latest_report = driver.find_elements_by_xpath(
+        f"//table[@id = 'DocumentCategoryDataList__ctl{section_num}_DocumentDataList']"
+        "//a[contains(@id, 'DocumentLinkButton')]"
+    )[-1]
+    default_fpath = os.path.join(OUTPUT_FOLDER, f"{latest_report.text}.xlsx")
+    latest_report.click()
+    altered_fpath = os.path.join(OUTPUT_FOLDER, "Maui SQMQ Report.xlsx")
+    os.rename(default_fpath, altered_fpath)  # overwrites previous copy
+    
+    print("Finished: by_largest_users\n")
+    driver.quit()
+    
+    
+def by_selected_major_users():
+    print("Starting: by_selected_major_users")
+    welded_pt_to_meters = {
+        'EGC30701': ['30701', '30703'],
+        'KUR33601': ['33601'],
+        'LCF20010': ['20001'],
+        'LCF20011': ['20021', '20022'],
+        'MRV16301': ['16301'],
+        'MUT19001': ['19001'],
+        'PHT04902': ['4921'],
+        'TAC31001': ['9931003'],
+        'TIR33501': ['33501'],
+        'BAL08201': ['8201'],
+        'BAL09626': ['9626'],
+        'GLB03401': ['3401', '3402'],
+        'KIN04310': ['4301', '4302'],
+        'MSD01801': ['1801', '1803'],
+        'STR00521': ['521', '522'],
+        'TCC00201': ['201', '202'],
+        'TRC02003': ['2003', '2004']
+    }
+    
+    driver = get_driver()
+    driver.get(
+        "https://www.oatis.co.nz/Ngc.Oatis.UI.Web.Internet/Common/"
+        "OatisLogin.aspx"
+    )
+    # 1. Go to 'First gas'
+    driver.find_element_by_id('NGCIX').click()
+    # 2. Select 'Daily Delivery Report (DDR) Details Page'
+    driver.find_element_by_id('DDRReportLinkButton').click()
+    # 3. Select the latest month
+    month_dropdown = Select(driver.find_element_by_id('MonthDropDown'))
+    dates = [datetime.strptime(l.text, "%b %Y") for l in month_dropdown.options
+             if l.text]
+    latest_date = max(dates)
+    latest_date_str = datetime.strftime(latest_date, '%b %Y')
+    month_dropdown.select_by_visible_text(latest_date_str)
+    # 4. Select welded points/IDs for the latest month
+    for welded_pt in welded_pt_to_meters.keys():
+        print(f"{welded_pt}: ", end='', flush=True)
+        for meter in welded_pt_to_meters[welded_pt]:
+            print(f"{meter}, ", end='', flush=True)
+            try:
+                welded_pt_dropdown = Select(
+                    WebDriverWait(driver, 2).until(
+                        EC.presence_of_element_located((By.ID, 'WeldedPointCodes'))
+                    )
+                )
+                welded_pt_dropdown.select_by_visible_text(welded_pt)
+                meter_dropdown = Select(
+                    WebDriverWait(driver, 2).until(
+                        EC.presence_of_element_located((By.ID, 'MeterGroupCodes'))
+                    )
+                )
+                meter_dropdown.select_by_visible_text(meter)
+            except TimeoutException:
+                raise NotImplementedError("Page has changed or is very slow to load")
+            # Download the data for each meter, replacing the previous versions
+            download_fpath = os.path.join(OUTPUT_FOLDER, f"DDR{meter}.csv")
+            if os.path.exists(download_fpath):
+                os.remove(download_fpath)
+            driver.find_element_by_id('DownloadButton').click()
+        print()
+    
+    print("Finished: selected_major_users\n")
+    driver.quit()
+    
+
+if __name__ == "__main__":
+    OUTPUT_FOLDER = os.path.join(os.getcwd(), "First Gas")
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
+    
+    by_largest_users()
+    by_selected_major_users()
---
+++ scrape_infoshare.py
@@ -0,0 +1,196 @@
+# There is three levels - type, group, dataset
+# This script navigates all 3 levels to the next dataset and recursively breaks
+# back up to the next level (and looks for next element) until there is no 
+# first-level elements left.
+# >> It is acting like a depth-first search <<
+
+# To setup Firefox driver:
+# https://askubuntu.com/questions/870530/how-to-install-geckodriver-in-ubuntu
+
+import os
+import csv
+import pandas as pd
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait, Select
+
+
+# Helper functions
+def get_driver_with_site():
+    opts = webdriver.FirefoxOptions()
+    opts.add_argument("--headless")
+    # Profile preferences necessary if downloading data+meta together in one CSV:
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.manager.showWhenStarting", False)
+    profile.set_preference("browser.download.dir", SAVE_DIR)
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv")
+
+    driver = webdriver.Firefox(options=opts, firefox_profile=profile)
+    driver.get("http://infoshare.stats.govt.nz/")
+    return driver
+
+
+def get_num_fl_folders():
+    driver = get_driver_with_site()
+    num_fl_folders = len(driver.find_elements_by_xpath(
+        "//div[@id = 'ctl00_MainContent_tvBrowseNodes']"
+        "//td[@style]"
+        "/a[starts-with(@id, 'ctl00_MainContent_tvBrowseNodest')]"
+    ))
+    driver.quit()
+    return num_fl_folders
+
+
+# Workhorse functions
+def get_info_from_dataset(driver, tl_elem, dataset_name, save_dir):
+    tl_elem.click()  # navigate to Search tab
+    try:
+        _ = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, 
+                "//table[@id = 'navigation']"
+            ))
+        )
+    except TimeoutException:
+        print("page didn't load :(")
+        return driver
+    
+    select_all_ids = [e.get_attribute('id') for e in driver.find_elements_by_xpath(
+        "//table[@id = 'ctl00_MainContent_tblVariableSelectors']"
+        "//span[substring(@id, string-length(@id) - string-length('SelectAll') + 1) = 'SelectAll']"
+    )]
+    for select_all_id in select_all_ids:
+        elem = driver.find_element_by_xpath(
+            "//table[@id = 'ctl00_MainContent_tblVariableSelectors']"
+            f"//span[@id = '{select_all_id}']"
+        )
+        elem.click()
+    
+    go_button = driver.find_element_by_xpath(
+        "//input[@id = 'ctl00_MainContent_btnGo']"
+    )
+    go_button.click()  # navigate to View table page
+    
+    # Saves data and metadata together in one CSV:
+#     save_dropdown = Select(driver.find_element_by_id('ctl00_MainContent_dlSaveOptions'))
+#     save_dropdown.select_by_value('csv')
+#     print('Downloaded CSV, ', end="")
+#     # Change CSV to have meaningful (comes out coded by default)
+#     fpath = max([f"{save_dir}/{f}" for f in os.listdir(save_dir)], key=os.path.getctime)
+#     os.rename(fpath, f"{save_dir}/{dataset_name}.csv")
+    
+    # Save data and metdata in different files:
+    tables = driver.find_elements_by_xpath("//table[@class = 'pxtable']")
+    data = tables[0].get_attribute('outerHTML')
+    data_soup = BeautifulSoup(data, 'html.parser')
+    data_df = pd.read_html(str(data_soup))[0]
+    data_df.to_csv(f"{save_dir}/{dataset_name}.csv")
+    
+    meta = tables[1].find_element_by_xpath(
+        "//td[@class = 'footnote']"
+    ).get_attribute('outerHTML')
+    meta_soup = BeautifulSoup(meta, 'html.parser')
+    meta_text = meta_soup.get_text(separator='\n')
+    with open(f"{save_dir}/{dataset_name}__meta.txt", 'w') as f:
+        f.write(meta_text)
+    
+    # Return to original page
+    try:
+        browse = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH,
+                "//table[@id = 'navigation']"
+                "//a[@id = 'ctl00_headerUserControl_browseHyperlink']"
+            ))
+        )
+        browse.click()
+    except TimeoutException:
+        driver.quit()  # restart browser
+        driver = get_driver_with_site()
+        
+    return driver
+
+
+def navigate_mainpage(save_dir):
+    # Don't want to expand ALL folders in same driver instance because selenium
+    # has a maximum scrolling ability, so a new driver/browser is used when
+    # navigating each first-level folder.
+    num_fl_folders = get_num_fl_folders()
+    
+    data_ids = []
+    i = 0
+#     for i in range(3, 4):
+    for i in range(num_fl_folders):
+        driver = get_driver_with_site()
+        try:
+            fl_elem = driver.find_element_by_xpath(
+                "//div[@id = 'ctl00_MainContent_tvBrowseNodes']"
+                f"//a[@id = 'ctl00_MainContent_tvBrowseNodest{i}']"
+            )
+        except NoSuchElementException:
+            print('y1')
+            break  # no more first-level elements
+        fl_name = fl_elem.text
+        print(i, fl_name)
+        fl_elem.click()
+        fl_num = i
+        i += 1
+
+        while True:
+            try:
+                sl_elem = WebDriverWait(driver, 2).until(
+                    EC.presence_of_element_located((By.XPATH, 
+                        "//div[@id = 'ctl00_MainContent_tvBrowseNodes']"
+                        f"/div[@id = 'ctl00_MainContent_tvBrowseNodesn{fl_num}Nodes']"
+                        f"//a[@id = 'ctl00_MainContent_tvBrowseNodest{i}']"
+                    ))
+                )
+            except TimeoutException:
+                print('y2')
+                break  # no more second-level elements
+            sl_name = sl_elem.text
+            print(f"  > {sl_name}:", end=' ', flush=True)
+            sl_elem.click()
+            sl_num = i
+            i += 1
+
+            while True:
+                try:
+                    tl_elem = WebDriverWait(driver, 2).until(
+                        EC.presence_of_element_located((By.XPATH,
+                            "//div[@id = 'ctl00_MainContent_tvBrowseNodes']"
+                            f"/div[@id = 'ctl00_MainContent_tvBrowseNodesn{fl_num}Nodes']"
+                            f"/div[@id = 'ctl00_MainContent_tvBrowseNodesn{sl_num}Nodes']"
+                            f"//a[@id = 'ctl00_MainContent_tvBrowseNodest{i}']"
+                        ))
+                    )
+                except TimeoutException:
+                    print('y3')
+                    break  # no more third-level elements
+
+                data_ids.append(tl_elem.get_attribute('id'))
+                print(i, end=' ', flush=True)
+
+                # LOGIC TO DOWNLOAD DATASET:
+                tl_name = tl_elem.text
+                dataset_name = '__'.join([fl_name, sl_name, tl_name])
+                driver = get_info_from_dataset(driver, tl_elem, dataset_name, save_dir)
+
+                i += 2  # unsure why this is different to first/second levels 
+
+        driver.quit()
+
+    return data_ids
+
+
+if __name__ == "__main__":
+    SAVE_DIR = os.path.join(os.getcwd(), "infoshare_downloads")
+    if not os.path.exists(SAVE_DIR):
+        os.makedirs(SAVE_DIR)
+    
+    data_ids = navigate_mainpage(SAVE_DIR)
+    
+    print("Number of datasets:", len(data_ids))
